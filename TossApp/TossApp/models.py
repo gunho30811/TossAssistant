2d55ad1e4370c11b8d13108d6cff26ae31bf8915
LAGUNHO <100767288+gunho30811@users.noreply.github.com>
2024-02-15 05:31:02 +0900

    Django 모델 구축

---
+++ TossApp/TossApp/models.py
@@ -0,0 +1,41 @@
+from django.db import models
+from django.contrib.postgres.fields import JSONField
+
+
+class SidebarMenu(models.Model):
+    code_name = models.CharField(max_length=255)
+    title = models.CharField(max_length=255)
+    order_number = models.IntegerField()
+    parent_id = models.IntegerField(null=True, blank=True)
+    target = models.CharField(max_length=255, blank=True, null=True)
+    tenant_id = models.IntegerField()
+
+
+class ContainerMetadata(models.Model):
+    code_name = models.CharField(max_length=255)
+    title = models.CharField(max_length=255)
+    type = models.CharField(max_length=50, blank=True, null=True)
+    company = models.CharField(max_length=255, blank=True, null=True)
+    children = JSONField(blank=True, null=True)
+    tenant_id = models.IntegerField()
+
+
+class TableMetadata(models.Model):
+    code_name = models.CharField(max_length=255)
+    title = models.CharField(max_length=255)
+    type = models.CharField(max_length=50, blank=True, null=True)
+    source = models.CharField(max_length=255, blank=True, null=True)
+    company = models.CharField(max_length=255, blank=True, null=True)
+    columns = JSONField(blank=True, null=True)
+    tenant_id = models.IntegerField()
+
+
+class FormMetadata(models.Model):
+    code_name = models.CharField(max_length=255)
+    company = models.CharField(max_length=255, blank=True, null=True)
+    steps = JSONField(blank=True, null=True)
+    initial_step = models.CharField(max_length=50, blank=True, null=True)
+    actions = JSONField(blank=True, null=True)
+    feedback = JSONField(blank=True, null=True)
+    masked_parameter = JSONField(blank=True, null=True)
+    tenant_id = models.IntegerField(null=True, blank=True)
