7ad37ffbcf1aab2e1a0413fe6cc037cdc5ef2246
LAGUNHO <100767288+gunho30811@users.noreply.github.com>
2024-02-15 16:28:44 +0900

    sidebar styled

--- my-toss/src/components/Sidebar.tsx
+++ my-toss/src/components/Sidebar.tsx
@@ -1,5 +1,10 @@
+/** @jsxImportSource @emotion/react */
+import { css, useTheme } from '@emotion/react';
+import styled from '@emotion/styled';
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import logo from '../Toss_Logo_Secondary_Gray.png';
+
 
 interface MenuItem {
   id: number;
@@ -11,30 +16,69 @@ interface MenuItem {
   tenant_id: number;
 }
 
+const SidebarContainer = styled.div`
+  background: #f4f7fa;
+  min-width: 250px;
+  max-width: 250px;
+  height: 100vh;
+  padding: 20px;
+`;
+
+const TopSection = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const Logo = styled.img`
+  max-width: 40%;
+  height: auto;
+`;
+
+const AdminText = styled.div`
+  font-size: 16px;
+`;
+
+const SidebarList = styled.ul`
+  list-style: none;
+  padding: 0;
+  margin: 0;
+`;
+
+const SidebarItem = styled.li`
+  color: #333;
+  text-align: left;
+  padding-left:20px;
+  font-size:14px;
+`;
+
 const Sidebar: React.FC = () => {
-  const [menuItems, setMenuItems] = useState<MenuItem[]>([]);
+    const [menuItems, setMenuItems] = useState<MenuItem[]>([]);
+  
+    useEffect(() => {
+      axios.get('/api/sidebar-menu/')
+        .then(response => {
+          setMenuItems(response.data);
+        })
+        .catch(error => {
+          console.error('There was an error fetching the menu items', error);
+        });
+    }, []);
 
-  useEffect(() => {
-    axios.get('/api/sidebar-menu/')
-      .then(response => {
-        setMenuItems(response.data);
-      })
-      .catch(error => {
-        console.error('There was an error fetching the menu items', error);
-      });
-  }, []);
-
-  return (
-    <div className="sidebar">
-      <ul>
-        {menuItems.map((item: MenuItem) => (
-          <li key={item.id}>
-            {item.title}
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+    return (
+        <SidebarContainer>
+          <TopSection>
+            <Logo src={logo} alt="Toss Logo" />
+            <AdminText>product admin</AdminText>
+          </TopSection>
+          <SidebarList>
+            {menuItems.map((item: MenuItem) => (
+              <SidebarItem key={item.id}>
+                {item.title}
+              </SidebarItem>
+            ))}
+          </SidebarList>
+        </SidebarContainer>
+      );
 };
 
 export default Sidebar;
