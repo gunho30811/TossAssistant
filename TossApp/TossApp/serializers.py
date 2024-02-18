c648587cee4eb28d5461a50f445b3665bd24580a
LAGUNHO <100767288+gunho30811@users.noreply.github.com>
2024-02-18 23:50:28 +0900

    Container.tsx 관련
    모델, api 뷰, 시리얼라이저 설정

--- TossApp/TossApp/serializers.py
+++ TossApp/TossApp/serializers.py
@@ -1,8 +1,13 @@
 # Django serializers.py
 from rest_framework import serializers
-from .models import SidebarMenu
+from .models import SidebarMenu, ContainerMetadata
 
 class SidebarMenuSerializer(serializers.ModelSerializer):
     class Meta:
         model = SidebarMenu
+        fields = '__all__'
+
+class ContainerMetadataSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ContainerMetadata
         fields = '__all__'
--- TossApp/TossApp/views.py
+++ TossApp/TossApp/views.py
@@ -1,8 +1,15 @@
 # Django views.py
 from rest_framework import generics
-from .models import SidebarMenu
-from .serializers import SidebarMenuSerializer
+from .models import SidebarMenu, ContainerMetadata
+from .serializers import SidebarMenuSerializer, ContainerMetadataSerializer
 
+# 기존의 SidebarMenuListAPIView
 class SidebarMenuListAPIView(generics.ListAPIView):
     queryset = SidebarMenu.objects.all()
     serializer_class = SidebarMenuSerializer
+
+# 추가할 ContainerMetadataListAPIView
+class ContainerMetadataListAPIView(generics.ListAPIView):
+    queryset = ContainerMetadata.objects.all()
+    serializer_class = ContainerMetadataSerializer
+
