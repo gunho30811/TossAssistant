e116b02617ac498605baf3e6d124c5acaa0dfe7f
LAGUNHO <100767288+gunho30811@users.noreply.github.com>
2024-02-18 23:52:02 +0900

    container.tsx 수정 및 App추가

--- my-toss/src/App.tsx
+++ my-toss/src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Sidebar from './components/Sidebar'; // Sidebar 컴포넌트를 임포트합니다.
+import Container from './components/container';
 
 function App() {
   return (
     <div className="App">
         <Sidebar />
+        <Container />
     </div>
   );
 }
