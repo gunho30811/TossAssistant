e3cbe07d0b1b05a93853bc163a7f00b1a0b29772
LAGUNHO <100767288+gunho30811@users.noreply.github.com>
2024-02-23 01:02:06 +0900

    Update container.tsx
    스타일 수정

--- my-toss/src/components/container.tsx
+++ my-toss/src/components/container.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import styled from '@emotion/styled';
 
 // ContainerMetadata 인터페이스
 interface ContainerMetadata {
@@ -8,9 +9,22 @@ interface ContainerMetadata {
   title: string;
   type?: string;
   company?: string;
-  children?: any;  // JSONField에 대한 타입은 any로 지정, 더 정확한 타입 지정 필요시 수정
+  children?: any; // JSONField에 대한 타입은 any로 지정
   tenant_id: number;
 }
+
+// Styled Components
+const ContainerWrapper = styled.div`
+  position: absolute;
+  bottom:90vh;
+  left:350px;
+  border-radius: 8px; // 둥근 모서리를 추가합니다
+`;
+
+const Title = styled.h1`
+    text-align: left;
+    color: #333; // 제목 색상을 설정합니다
+`;
 
 // Container 컴포넌트
 const Container: React.FC = () => {
@@ -26,16 +40,15 @@ const Container: React.FC = () => {
       });
   }, []);
 
-  // 데이터를 웹페이지에 렌더링하는 방법
   return (
-    <div>
+    <ContainerWrapper>
       {containerData.map((data: ContainerMetadata) => (
         <div key={data.id}>
-          <h1>{data.title}</h1>
-          {/* 여기에 children 데이터를 렌더링하는 로직 추가 */}
+          <Title>{data.title}</Title>
+          {/* children 데이터를 렌더링하는 로직 추가 */}
         </div>
       ))}
-    </div>
+    </ContainerWrapper>
   );
 };
 
